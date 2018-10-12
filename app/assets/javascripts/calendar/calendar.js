e9e6742092741870d02fa35eca7991b315df332c
Radoslaw Wasiuk <radoslaw.wasiuk@spartez.com>
2018-10-12 08:23:38 +0200

    calendar js component - initial version

---
+++ app/assets/javascripts/calendar/calendar.js
@@ -0,0 +1,30 @@
+function CalendarRenderer(calendar, calendarDomObject) {
+	this.render = function() {
+		calendar.clearContent();
+		calendar.days.forEach((day) => calendarDomObject.append(day.render()));
+	}
+};
+
+function Calendar(domElement, dayTemplate, eventTemplate) {
+	this.startDate = moment();
+	this.days = [];
+	this.renderer = new CalendarRenderer(this, domElement);
+
+	this.generateCalendarDays = function() {
+		let dateFromWhichStart = moment(this.startDate);
+		this.days = [];
+		for (let i=0; i<7; i++) {
+			let date = dateFromWhichStart.add(1, 'days');
+			let day = new Day(date, dayTemplate, eventTemplate);
+			day.addEvent('x');
+			this.days.push(day);
+		}
+	};
+	this.render = function() {
+		this.renderer.render();
+	};
+	this.clearContent = function() {
+		domElement.html('');
+	};
+	this.generateCalendarDays();
+};
